e4d0bd6f642f5609dfeb4e6c49e766fcc31e4db3
Vince John Perez <perezvj.social@gmail.com>
2022-04-25 17:02:48 +0800

    Update fastech.sql

--- assets/sql/fastech.sql
+++ assets/sql/fastech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2022 at 04:55 PM
+-- Generation Time: Apr 25, 2022 at 11:02 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -58,26 +58,22 @@ CREATE TABLE `appointments` (
   `time` varchar(50) DEFAULT NULL,
   `issue` varchar(255) NOT NULL,
   `ticket` varchar(255) NOT NULL,
-  `status` varchar(11) NOT NULL
+  `status` varchar(11) NOT NULL,
+  `notif_status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `appointments`
 --
 
-INSERT INTO `appointments` (`apt_id`, `requestee`, `name`, `phone`, `email`, `address`, `service`, `date`, `time`, `issue`, `ticket`, `status`) VALUES
-(65, 64, 'vincent halili', 12312312312, 'vincent@gmail.com', '123123', 'ORAL PROPHYLAXYS OR CLEANING', '2022-03-13', '5:13 PM', '123123', 'HUerlY/D8aQMJQ==', 'payment'),
-(66, 64, 'vincent halili', 12312312312, 'vincent@gmail.com', '123123321', 'DENTURES', '2022-03-22', '5:15 PM', '123123', '0fOB7CNAFdJopA==', 'declined'),
-(67, 64, 'vincent halili', 12312312321, 'vincent@gmail.com', '123123', 'ORAL PROPHYLAXYS OR CLEANING', '2022-03-23', '5:29 PM', '123123', '19mfVI1ylqY31A==', 'payment'),
-(68, 64, 'vincent halili', 12312312312, 'vincent@gmail.com', '123123', 'DENTURES', '2022-03-04', '10:27 AM', '123123', 'DSA/pztHyGBOxQ==', 'payment'),
-(69, 65, 'eugene halili', 9669435194, 'eugene@gmail.com', '123', 'DENTURES', '2022-03-16', '8:20 AM', '12321', 'zF4dns8DWvSz7A==', 'payment'),
-(71, 64, 'vincent halili', 9669435194, 'vincent@gmail.com', 'santolan', 'RESTORATION OR PASTA', '2022-03-15', '8:40 AM', '123123213', 'J1zS3PFTI7CPBA==', 'payment'),
-(72, 67, 'qwe rty', 9123123132, 'qwerty@gmail.com', 'pasig city', 'RESTORATION OR PASTA', '2022-03-10', '3:24 PM', 'sadasdasd', '3wizp3S5F3XA/A==', 'payment'),
-(73, 64, 'vincent halili', 96694354194, 'vincent@gmail.com', '135 santolan', 'ORAL PROPHYLAXYS OR CLEANING', '2022-03-30', '2:03 PM', '', 'AwOma70UqDlxtA==', 'approved'),
-(74, 64, 'vincent halili', 9123456789, 'vincent@gmail.com', 'santolan', 'RESTORATION OR PASTA', '2022-03-30', '11:35 AM', '123123', 'oR+5rgFKsxdRfQ==', 'approved'),
-(77, 68, 'vincent halili', 9123123123, 'vincent14@gmail.com', 'santolan', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-18', '10:48 AM', 'asdas', 'yDlePnJ9wEXqbg==', 'approve'),
-(78, 68, 'vincent halili', 9669435194, 'vincent14@gmail.com', 'santolan pasig city', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-20', '11:58 AM', '', 'Bf5Kx0rtFnARng==', 'approve'),
-(79, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', 'Quezon City', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-07', '9:30 AM', 'Cleaning Teeth', 'BIhKqagcni64WA==', 'approved');
+INSERT INTO `appointments` (`apt_id`, `requestee`, `name`, `phone`, `email`, `address`, `service`, `date`, `time`, `issue`, `ticket`, `status`, `notif_status`) VALUES
+(114, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', 'RESTORATION OR PASTA', '2022-04-26', '8:00 AM', 'asdasd', 'jnMoFV9SefKrFw==', 'approved', 1),
+(115, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', 'DENTURES', '2022-04-25', '8:00 AM', 'asdasdasd', '1XUyA+n6qCfmTw==', 'approved', 1),
+(116, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', '', '2022-04-25', '8:00 AM', 'asdasd', 'YChfX7oNVXX5IQ==', 'pending', 1),
+(120, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-26', '11:00 AM', '', 'VjYCQeyC0ThOsg==', 'pending', 1),
+(121, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-26', '9:00 AM', 'asdasdasd', 'ex1DlKHkM5puqg==', 'approved', 1),
+(122, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-27', '10:00 AM', 'agaefghafgadfgasdfgafad', 'J4SRaMKpN34nKw==', 'approved', 1),
+(123, 69, 'Vince John Perez', 9359148135, 'perezvj.social@gmail.com', '5315 Flora Vista, Peacock St., Brgy. Commonwealth', 'ORAL PROPHYLAXYS OR CLEANING', '2022-04-28', '8:00 AM', 'SDFSADGASD', 'tlwvhnrwXg7ijQ==', 'pending', 1);
 
 -- --------------------------------------------------------
 
@@ -97,7 +93,8 @@ CREATE TABLE `password_resets` (
 
 INSERT INTO `password_resets` (`id`, `email`, `token`) VALUES
 (9, 'perezvj.social@gmail.com', 'b34ebd983485d196c0841caf9eff62b86cdee950e94dcbc4f8c17785da8d144c3979da43ee124ff16a2af6b9a38e2d5d3083'),
-(10, 'perezvj.social@gmail.com', '5ee47fb5023ecee505593aeb29580802c9372d8eef88202fc3cfd4696a4cd91f9b8868c2d77270bb94336aee7b2a17e6bec9');
+(10, 'perezvj.social@gmail.com', '5ee47fb5023ecee505593aeb29580802c9372d8eef88202fc3cfd4696a4cd91f9b8868c2d77270bb94336aee7b2a17e6bec9'),
+(11, 'perezvj.social@gmail.com', '48d6eee2fcbc75f2758873f776dd1f83fcb6a535f97c1d85124dfdde091e80f520d876a54387eb1e9f65a9eba1cc74e25be2');
 
 -- --------------------------------------------------------
 
@@ -124,7 +121,23 @@ INSERT INTO `registered_accounts` (`id`, `firstname`, `lastname`, `email`, `pass
 (66, 'anthon', 'marquez', 'anthon@gmail.com', '4297f44b13955235245b2497399d7a93', 12312312312),
 (67, 'qwe', 'rty', 'qwerty@gmail.com', '4297f44b13955235245b2497399d7a93', 9669435194),
 (68, 'vincent', 'halili', 'vincent14@gmail.com', '4297f44b13955235245b2497399d7a93', 9669435194),
-(69, 'Vince John', 'Perez', 'perezvj.social@gmail.com', '8f18d7c7390e1be8edb8b0013f75e0cc', 9359148135);
+(69, 'Vince John', 'Perez', 'perezvj.social@gmail.com', '8f18d7c7390e1be8edb8b0013f75e0cc', 9359148135),
+(70, 'Vince John', 'Perez', 'perezvj14@gmail.com', '356531c7cf37111656f9e782b7c5efa5', 9359148135),
+(71, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(72, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(73, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(74, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(75, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(76, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(77, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(78, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(79, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(80, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(81, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(82, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(83, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(84, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0),
+(85, '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', 0);
 
 --
 -- Indexes for dumped tables
@@ -170,19 +183,19 @@ ALTER TABLE `admin_account`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `apt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `apt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 
 --
 -- AUTO_INCREMENT for table `password_resets`
 --
 ALTER TABLE `password_resets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `registered_accounts`
 --
 ALTER TABLE `registered_accounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- Constraints for dumped tables
